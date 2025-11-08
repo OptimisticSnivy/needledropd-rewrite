ecdd3307200cf95b50dc086a56c97eeae8423341
Anand Mahamuni <optimisticsnivy7203@proton.me>
2025-11-08 16:05:44 +0530

    Theme change to Slate & fixed Navbar!

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -12,25 +12,31 @@ import { InputWithButton } from "./inputWithButton";
 
 export default function Navbar() {
 	return (
-		<div className="flex flex-col mt-5 mx-auto w-7xl">
+		<div className="flex flex-col mt-3 w-2xl mx-auto">
 			<NavigationMenu>
-				<NavigationMenuList className="flex flex-row">
-					<NavigationMenuItem className="md:p-4 p-4 font-extrabold text-base font-mono">needledropd.</NavigationMenuItem>
-					<div className="flex flex-row">
-						<NavigationMenuItem>
-							<NavigationMenuLink className="hover:text-[#e53935]">Profile</NavigationMenuLink>
-						</NavigationMenuItem>
-						<NavigationMenuItem>
-							<NavigationMenuLink className="hover:text-[#e53935]">Artists</NavigationMenuLink>
-						</NavigationMenuItem>
-						<NavigationMenuItem>
-							<NavigationMenuLink className="hover:text-[#e53935]">Albums</NavigationMenuLink>
-						</NavigationMenuItem>
+				<div className="flex">
+					<div className="grow-0 ...">
+						<NavigationMenuList>
+							<NavigationMenuItem className="p-1.5 md:p-1.5 font-extrabold text-base font-mono">needledropd.</NavigationMenuItem>
+							<NavigationMenuItem>
+								<NavigationMenuLink className="">Profile</NavigationMenuLink>
+							</NavigationMenuItem>
+							<NavigationMenuItem>
+								<NavigationMenuLink className="">Artists</NavigationMenuLink>
+							</NavigationMenuItem>
+							<NavigationMenuItem>
+								<NavigationMenuLink className="">Albums</NavigationMenuLink>
+							</NavigationMenuItem>
+						</NavigationMenuList>
 					</div>
-					{/* <div className="ml-auto md:ml-auto"> */}
-					{/* 	<InputWithButton /> */}
-					{/* </div> */}
-				</NavigationMenuList>
+					<div className="grow w-128 ...">
+					</div>
+					<div className="grow-0 w-64 ...">
+						<NavigationMenuList>
+							<InputWithButton />
+						</NavigationMenuList>
+					</div>
+				</div>
 			</NavigationMenu>
 		</div>
 	);
