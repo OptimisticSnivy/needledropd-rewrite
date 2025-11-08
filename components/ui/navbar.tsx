2ac3291309f3f0d37b4853a28d74956e24a286c2
Anand Mahamuni <optimisticsnivy7203@proton.me>
2025-11-08 17:20:34 +0530

    Navbar has menu for mobile displays.(doesnt func yet)

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -8,30 +8,35 @@ import {
 	NavigationMenuTrigger,
 	NavigationMenuViewport,
 } from "@/components/ui/navigation-menu"
+import { MenuIcon } from "lucide-react"
+import { Button } from "./button";
 import { InputWithButton } from "./inputWithButton";
 
 export default function Navbar() {
 	return (
-		<div className="p-0 md:p-1.5 flex flex-col mt-3 w-1 md:w-1 lg:w-1 mx-auto">
+		<div className="flex flex-col mt-7 md:mt-5 lg:mt-5 md:w-1 lg:w-1 mx-auto">
 			<NavigationMenu>
 				<div className="flex">
 					<div className="grow-0 md:grow-0 lg:grow-0">
 						<NavigationMenuList>
-							<NavigationMenuItem className=" font-extrabold text-base font-mono">needledropd.</NavigationMenuItem>
+							<NavigationMenuItem className="p-3 md:hidden lg:hidden font-extrabold text-base font-mono">
+								<Button><MenuIcon className="h-4 w-4" /></Button>
+							</NavigationMenuItem>
+							<NavigationMenuItem className="font-extrabold text-base font-mono">needledropd.</NavigationMenuItem>
 							<NavigationMenuItem>
-								<NavigationMenuLink className="">Profile</NavigationMenuLink>
+								<NavigationMenuLink className="hidden md:block lg:block">Profile</NavigationMenuLink>
 							</NavigationMenuItem>
 							<NavigationMenuItem>
-								<NavigationMenuLink className="">Artists</NavigationMenuLink>
+								<NavigationMenuLink className="hidden md:block lg:block">Artists</NavigationMenuLink>
 							</NavigationMenuItem>
 							<NavigationMenuItem>
-								<NavigationMenuLink className="">Albums</NavigationMenuLink>
+								<NavigationMenuLink className="hidden md:block lg:block">Albums</NavigationMenuLink>
 							</NavigationMenuItem>
 						</NavigationMenuList>
 					</div>
-					<div className="md:grow md:w-32 lg:grow lg:w-128">
+					<div className="grow md:grow md:w-32 lg:grow lg:w-128">
 					</div>
-					<div className="md:grow-0 md:w-64 lg:grow-0 lg:w-64">
+					<div className="hidden md:block lg:block md:grow-0 md:w-64 lg:grow-0 lg:w-64 ">
 						<NavigationMenuList>
 							<InputWithButton />
 						</NavigationMenuList>
@@ -41,3 +46,4 @@ export default function Navbar() {
 		</div>
 	);
 }
+
