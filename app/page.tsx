d3d6744856358f2dcf4acbb37ff26e9c30dc9164
Anand Mahamuni <optimisticsnivy7203@proton.me>
2025-11-10 13:32:25 +0530

    Fixed responsiveness for home

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import AlbumCard from "@/components/ui/album-card";
 
 export default function Home() {
 	return (
-		<div className="flex mt-15 w-8xl mx:auto justify-center">
-			<div className="flex flex-row flex-wrap mt:5 mx:auto justify-center">
+		<div className="flex mt-15 md:w-4xl md:justify-center md:mx-auto lg:justify-center lg:w-6xl lg:mx-auto">
+			<div className="flex flex-row flex-wrap mt:5 justify-center">
 				<AlbumCard ARTIST="Radiohead" ANAME="Amnesiac" />
 				<AlbumCard ARTIST="Radiohead" ANAME="Hail to the Thief" />
 				<AlbumCard ARTIST="Radiohead" ANAME="Kid A" />
@@ -13,6 +13,10 @@ export default function Home() {
 				<AlbumCard ARTIST="Radiohead" ANAME="A Moon Shaped Pool" />
 				<AlbumCard ARTIST="Radiohead" ANAME="In Rainbows" />
 				<AlbumCard ARTIST="Radiohead" ANAME="Ok Computer" />
+				<AlbumCard ARTIST="Radiohead" ANAME="Pablo Honey" />
+				<AlbumCard ARTIST="Radiohead" ANAME="I Might Be Wrong (Live)" />
+				<AlbumCard ARTIST="Caroline Polachek" ANAME="Pang" />
+				<AlbumCard ARTIST="Charli XCX" ANAME="Brat" />
 			</div>
 		</div>
 	);
